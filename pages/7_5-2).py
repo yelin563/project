f6dba7386adaea63527422239f388e27f795a361
yelin563 <126746403+yelin563@users.noreply.github.com>
2024-10-06 16:51:01 +0900

    Update 7_5-2).py

--- pages/7_5-2).py
+++ pages/7_5-2).py
@@ -7,7 +7,7 @@ from streamlit_folium import st_folium, folium_static
 import matplotlib
 import matplotlib.pyplot as plt
 from folium.plugins import Draw
-from streamlit_gsheets import GSheetsConnection
+
 import requests
 from branca.colormap import linear
 from scipy.spatial import Voronoi, voronoi_plot_2d
