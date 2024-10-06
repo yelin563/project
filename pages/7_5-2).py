6a7b11fad306f2271f4cc5cf2e3f843772b73fd3
yelin563 <126746403+yelin563@users.noreply.github.com>
2024-10-06 17:05:13 +0900

    Update 7_5-2).py

--- pages/7_5-2).py
+++ pages/7_5-2).py
@@ -48,7 +48,7 @@ def voronoi_finite_polygons_2d(vor, radius=None):
 
     center = vor.points.mean(axis=0)
     if radius is None:
-        radius = np.ptp(vor.points, axis=0).max()
+        radius = np.ptp(vor.points) 
 
     # Construct a map containing all ridges for a given point
     all_ridges = {}
