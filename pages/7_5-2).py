cf431d4cd923128d9947b3c4c5c9a1ea7447e221
yelin563 <126746403+yelin563@users.noreply.github.com>
2024-10-06 16:33:00 +0900

    Update 7_5-2).py

--- pages/7_5-2).py
+++ pages/7_5-2).py
@@ -182,6 +182,7 @@ if 'map' not in st.session_state:
 if 'run' not in st.session_state:
     st.session_state.run = 'N'
 # 지도 표시
+
 # folium 지도 데이터를 st_folium으로 받음
 map_data = st_folium(st.session_state.map, width=800, height=600)
 
